b3f0d65dd5cde0b27c91865d408f1c8559ec6a76
dinesha <dineshapriyadarshani96@gmail.com>
2023-03-22 22:38:37 +0530

    Updates

---
+++ precache-manifest.91866357cee85fc7517635570fd25ed9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6652fc6fff69114044cd",
+    "url": "/BMI-Calculator/static/css/main.f7144255.chunk.css"
+  },
+  {
+    "revision": "6652fc6fff69114044cd",
+    "url": "/BMI-Calculator/static/js/main.947d0ad7.chunk.js"
+  },
+  {
+    "revision": "75e1486cca2829dcbbb1",
+    "url": "/BMI-Calculator/static/js/runtime~main.a7f2cbe6.js"
+  },
+  {
+    "revision": "c09dcdedd5d4f80e623e",
+    "url": "/BMI-Calculator/static/css/2.01c3f3ac.chunk.css"
+  },
+  {
+    "revision": "c09dcdedd5d4f80e623e",
+    "url": "/BMI-Calculator/static/js/2.4dbc8623.chunk.js"
+  },
+  {
+    "revision": "9089a225bc756d0e02b7",
+    "url": "/BMI-Calculator/static/js/3.25d10edb.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/BMI-Calculator/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/BMI-Calculator/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/BMI-Calculator/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/BMI-Calculator/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/BMI-Calculator/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "1b0ba52cdcc3e8c652e23981906dcf98",
+    "url": "/BMI-Calculator/index.html"
+  }
+];
